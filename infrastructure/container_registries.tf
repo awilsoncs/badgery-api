e757fa166253f3d4b2e9e2617c3bb33a5e995e38
Aaron Wilson <acwilsoncs@gmail.com>
2024-04-23 07:06:14 -0600

    tidy name

--- infrastructure/container_registries.tf
+++ infrastructure/container_registries.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "bar"
+resource "aws_ecr_repository" "badgery_api" {
+  name                 = "badgery_api_aws_ecr_repository"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
